8523296351cb023cbf18145e8abf32b53e2eb96c
Nomanciit <numanyousafzai123@gmai.com>
2016-04-25 17:42:01 +0500

    my first commit

---
+++ Labwork/src/Labwork/Main.java
@@ -0,0 +1,15 @@
+package Labwork;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Hell world");
+		System.out.println("Github learning");
+		
+	}
+
+}
